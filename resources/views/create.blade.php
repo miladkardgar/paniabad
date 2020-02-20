c055e10216fbb6be089594da09a6b035132551d8
Milad Kardgar <mk.kardgar@gmail.com>
2020-02-20 19:50:44 +0330

    create layout

--- resources/views/create.blade.php
+++ resources/views/create.blade.php
@@ -1,9 +1,9 @@
-@extend('layout.master')
+@extends('layout.master')
 @section('content')
-<form action="{{route('createAccount')}}" method="post">
-    @scrf
-<label for="name">نام حساب</label>
-<input class="input" type="text" name="name">
-<button type="submit" class="btn">افزودن</button>
-</form>
+    <form action="{{route('createAccount')}}" method="post">
+        @csrf
+        <label for="name">نام حساب</label>
+        <input class="input" type="text" name="name">
+        <button type="submit" class="btn">افزودن</button>
+    </form>
 @endsection
