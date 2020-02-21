87088b3ca3ada7dd1f00c7e41f770c99bc0c8c8a
Milad Kardgar <mk.kardgar@gmail.com>
2020-02-21 21:03:07 +0330

    create layout

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -7,15 +7,23 @@ use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Http\Request;
 use Illuminate\Routing\Controller as BaseController;
+use Symfony\Component\Process\Exception\ProcessFailedException;
+use Symfony\Component\Process\Process;
+
 class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
 
 
-    public function createAcount(){
+    public function createAcount()
+    {
         return view('create');
     }
-    public function storeAccount(Request $request){
-        exec('cd /etc/ && ls');
+
+    public function storeAccount(Request $request)
+    {
+        putenv('name=' . $request['name']);
+        var_dump(shell_exec('/etc/openvpn-install/newUser1.sh'));
+        echo "downloadLink:"."<a href='http://5.152.193.3/".$request['name'].">Download</a>";
     }
 }
